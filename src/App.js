ce5481ae5e546eeba9c3e769f1badfd9d0d2f372
anas599 <anas599@yahoo.com>
2023-03-06 23:26:58 +0300

    Add Navlink to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import {
+  NavLink, Routes, Route,
+} from 'react-router-dom';
 import logo from '../assets/logo.png';
+import MyProfile from './MyProfile';
+import Missions from './Missions';
+import Rockets from './Rockets';
 
 const Header = () => (
   <header>
@@ -9,11 +15,28 @@ const Header = () => (
     </div>
     <nav>
       <ul className="menuLinks">
-        <li>Rockets</li>
-        <li>Missions</li>
-        <li>My Profile</li>
+        <li>
+          <NavLink to="/Rockets" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Missions" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            Missions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/MyProfile" style={({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' })}>
+            My Profile
+          </NavLink>
+        </li>
       </ul>
     </nav>
+    <Routes>
+      <Route path="/Rockets" element={<Rockets />} />
+      <Route path="/MyProfile" element={<MyProfile />} />
+      <Route path="/Missions" element={<Missions />} />
+    </Routes>
   </header>
 );
 
