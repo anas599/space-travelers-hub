72f41c1f1eb04547c7a35e264135f50026ca39c1
Kender ROMAIN <kenderromain@gmail.com>
2023-03-07 11:24:35 -0400

    Create SingleMission component.

---
+++ src/components/SingleMission.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SingleMission = ({ mission }) => (
+  <tr>
+    <td>{mission.missionName}</td>
+    <td>{mission.description}</td>
+    <td>
+      <div
+        className={mission?.joined ? 'member-badge active' : 'member-badge'}
+      >
+        {mission?.joined ? 'Active Member' : 'NOT A MEMBER'}
+      </div>
+    </td>
+    <td>
+      <button
+        className={mission.joined ? 'leaveBtn' : 'joinBtn'}
+        type="button"
+      >
+        {mission?.joined ? 'Leave Mission' : 'Join Mission'}
+      </button>
+    </td>
+  </tr>
+);
+SingleMission.propTypes = {
+  mission: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    missionName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    joined: PropTypes.bool,
+  }).isRequired,
+};
+export default SingleMission;
