3e6e34d37b48e1b1a87045916919e7529c023a69
Kender ROMAIN <kenderromain@gmail.com>
2023-03-07 11:50:04 -0400

    Add extra-reducer and action getMissionFromAPI.

--- src/redux/mission/missionSlice.jsx
+++ src/redux/mission/missionSlice.jsx
@@ -44,7 +44,7 @@ const missionSlice = createSlice({
   },
   extraReducers: {
     [getMissionFromAPI.pending]: (state) => {
-      console.log('Loading...', state);
+      console.log('Loading.....', state);
     },
     [getMissionFromAPI.fulfilled]: (state, action) => {
       const missions = action.payload.map(
