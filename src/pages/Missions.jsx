a2b553c8dd9f3080a2f2ab1697cc804ea21fecb5
Kender ROMAIN <kenderromain@gmail.com>
2023-03-07 11:29:28 -0400

    Add content for the Missions page.

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import SingleMission from '../components/SingleMission';
 
-const Missions = () => (
-  <div>Missions</div>
-);
-
+const Missions = () => {
+  const missions = [
+    {
+      id: 1,
+      missionName: 'Mission 1',
+      description: 'Mission Desc',
+    },
+  ];
+  return (
+    <section className="missionContainer">
+      <table className="missions">
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+        {missions.map((mission) => (
+          <SingleMission key={mission.id} mission={mission} />
+        ))}
+      </table>
+    </section>
+  );
+};
 export default Missions;
