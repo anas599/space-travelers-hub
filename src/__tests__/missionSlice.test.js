8b814fc94a9f7d4fce234ad5c4fe04b5fd26a36a
Kender ROMAIN <kenderromain@gmail.com>
2023-03-08 15:12:07 -0400

    Add tests for missionSlice

--- src/__tests__/missionSlice.test.js
+++ src/__tests__/missionSlice.test.js
@@ -2,7 +2,7 @@ import { getMissionFromAPI } from '../redux/mission/missionSlice';
 import store from '../redux/store';
 
 describe('Mission Slice', () => {
-  it('Should load all the mission and set the state', async () => {
+  it('Should load all the mission and set the state x', async () => {
     await store.dispatch(getMissionFromAPI());
     const state = store.getState();
     console.log(state);
